c0ff38727e2fdfeec32dd3be735b40a4d96d905c
uelionobre <uelionobre@hotmail.com>
2024-02-06 22:43:53 -0300

    Registrar models Budget, Marriage e Vendor

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Budget, Marriage, Vendor
+
+admin.site.register(Budget)
+admin.site.register(Marriage)
+admin.site.register(Vendor)
